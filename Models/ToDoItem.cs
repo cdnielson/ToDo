31114091262bd942b520be06b612b8be1af7cee1
cdnielson <cdnielson@folkprophet.com>
2015-11-07 11:59:19 -0700

    Needs Unit Testing

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace ToDo.Models
+{
+    public class ToDoItem
+    {
+        public int ID { get; set; }
+        public string Item { get; set; }
+        public bool Completed { get; set; }
+    }
+
+    public class ToDoDBContext : DbContext
+    {
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
